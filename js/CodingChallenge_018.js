7df4566af2324ededcd603423358396f341b5ebc
danielmbecker <dnnybecker@gmail.com>
2022-05-28 23:41:18 -0700

    Daily Coding Challenge

---
+++ js/CodingChallenge_018.js
@@ -0,0 +1,28 @@
+//  Ones and Zeros
+//  
+//  Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+//  Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+//  Examples:
+//  Testing: [0, 0, 0, 1] ==> 1
+//  Testing: [0, 0, 1, 0] ==> 2
+//  Testing: [0, 1, 0, 1] ==> 5
+//  Testing: [1, 0, 0, 1] ==> 9
+//  Testing: [0, 0, 1, 0] ==> 2
+//  Testing: [0, 1, 1, 0] ==> 6
+//  Testing: [1, 1, 1, 1] ==> 15
+//  Testing: [1, 0, 1, 1] ==> 11
+
+const binaryArrayToNumber = arr => {
+    const reversedArray= arr.reverse()
+    let pushedArray = []
+      
+      for (let i = 0; i < reversedArray.length; i++) { 
+        if (reversedArray[i] !==0){
+        let multiplier = (Math.pow(2, i))
+        pushedArray.push(multiplier)
+    }}
+    
+    let finalValue = pushedArray.reduce((acc, c) => acc + c, 0)
+    return finalValue
+    
+    };
